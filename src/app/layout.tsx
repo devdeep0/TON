d7d03872dd365770dd5bb248ee1fb7a45e5790a8
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-07-20 22:15:31 +0530

    added manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <TonConnectUIProvider manifestUrl="http://localhost:3000/tonconnect-manifest.json"
+      <TonConnectUIProvider manifestUrl="https://ton-g3gwzdv9r-devdeep0s-projects.vercel.app/tonconnect-manifest.json"
       walletsListConfiguration={{
         includeWallets: [
           {
